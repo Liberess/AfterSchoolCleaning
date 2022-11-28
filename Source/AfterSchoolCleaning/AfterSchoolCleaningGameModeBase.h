a2c3d8960ca42e9b3b71720eae1463b4048bef76
Liberess <58577796+Liberess@users.noreply.github.com>
2022-11-28 14:10:29 +0900

    FEAT UE4 Project

---
+++ Source/AfterSchoolCleaning/AfterSchoolCleaningGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AfterSchoolCleaningGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AFTERSCHOOLCLEANING_API AAfterSchoolCleaningGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
